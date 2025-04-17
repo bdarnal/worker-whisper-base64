56e36023039837a7a609b7f2d60f723c95bf3f0d
bdarnal <44588905+bdarnal@users.noreply.github.com>
2025-04-18 00:03:46 +0200

    fix: Update download_models.sh

--- builder/download_models.sh
+++ builder/download_models.sh
@@ -1,3 +1,2 @@
 mkdir ./weights
-wget https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt -P ./weights
 wget https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt -P ./weights
